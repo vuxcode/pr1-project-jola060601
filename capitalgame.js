3f7b019137a4ad0e55ad84204131f5a2cd8e75f3
jola060601 <jola060601@skovde.se>
2026-05-05 11:30:51 +0200

    Removed span from "capitalQuestion" variable

--- capitalgame.js
+++ capitalgame.js
@@ -19,7 +19,7 @@ var capitalModal = document.querySelector(".game-modal");
 // Get the correct <main> where the container box is displayed
 var capitalContainer = document.querySelector(".containerbox");
 // Get the correct <h1> where the question is displayed
-var capitalQuestion = document.querySelector("#capital-question span");
+var capitalQuestion = document.querySelector("#capital-question");
 // Create variables to find the button elements on the screen
 var button0 = document.getElementById("choice0");
 var button1 = document.getElementById("choice1");
